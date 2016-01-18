06bd680e110272e262b1ea14dd922b17bfb0b540
root <knzcnn@gmail.com>
2016-01-18 02:15:59 +0200

    added more examples

--- part-1.js
+++ part-1.js
@@ -3,26 +3,33 @@
 require("babel-core/register");
 
 let sym = Symbol("no more collisions");
-let sym1 = Symbol.for("global");
-let sym2 = Symbol.for("global");
+let sym1 = Symbol("no more collisions");
+
+console.log(sym === sym1); //false
 
 let myObj = {};
-
 myObj[sym] = "no more collisions";
-myObj[sym1] = "what value";
-myObj[sym2] = "will we get?";
+myObj[sym1] = "no more indeed";
+
+console.log(myObj[sym]); //no more collisions
+console.log(myObj[sym1]); //no more indeed
+
+let sym2 = Symbol.for("global");
+let sym3 = Symbol.for("global");
+
+myObj[sym2] = "what value";
+myObj[sym3] = "will we get?";
+
+console.log(myObj[sym2]); //will we get?
+console.log(myObj[sym3]); //will we get?
 
 myObj.someOtherKey1 = "some other key 1";
 myObj.someOtherKey2 = "some other key 2";
 myObj.someOtherKey3 = "some other key 3";
 
-console.log("\n\n");
-console.log("myObj[sym1]: ", myObj[sym1]); //will we get?
-console.log("myObj[sym2]: ", myObj[sym2]); //will we get?
-
-console.log("Object.keys: ", Object.keys(myObj)); //[ 'someOtherKey1', 'someOtherKey2', 'someOtherKey3' ]
-console.log("Object.getOwnPropertyNames: ", Object.getOwnPropertyNames(myObj)); //[ 'someOtherKey1', 'someOtherKey2', 'someOtherKey3' ]
-console.log("Object.getOwnPropertySymbols: ", Object.getOwnPropertySymbols(myObj)); //[ Symbol(no more collisions), Symbol(global) ]
+console.log(Object.keys(myObj)); //[ 'someOtherKey1', 'someOtherKey2', 'someOtherKey3' ]
+console.log(Object.getOwnPropertyNames(myObj)); //[ 'someOtherKey1', 'someOtherKey2', 'someOtherKey3' ]
+console.log(Object.getOwnPropertySymbols(myObj)); //[ Symbol(no more collisions), Symbol(no more collisions), Symbol(global) ]
 
 
 
